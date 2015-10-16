aa3612fa4cba5b40f451b6126e1a44bd64c8c405
Ashley Davis <ashley@codecapers.com.au>
2015-10-16 13:30:15 +1000

    Added SelectorNode.

---
+++ src/Nodes/SelectorNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fluent_behaviour_tree.Nodes
+{
+    /// <summary>
+    /// Selects the first node that succeeds. Tries successive nodes until it finds one that doesn't fail.
+    /// </summary>
+    public class SelectorNode : IBehaviourTreeNode
+    {
+        /// <summary>
+        /// List of child nodes.
+        /// </summary>
+        private List<IBehaviourTreeNode> children = new List<IBehaviourTreeNode>(); //todo: optimization, bake this to an array.
+
+        public BehaviourTreeStatus Tick(TimeData time)
+        {
+            foreach (var child in children)
+            {
+                var childStatus = child.Tick(time);
+                if (childStatus != BehaviourTreeStatus.Failure)
+                {
+                    return childStatus;
+                }
+            }
+
+            return BehaviourTreeStatus.Failure;
+        }
+
+        /// <summary>
+        /// Add a child node to the selector.
+        /// </summary>
+        public void AddChild(IBehaviourTreeNode child)
+        {
+            children.Add(child);
+        }
+    }
+}
---
+++ tests/SelectorNodeTests.cs
@@ -0,0 +1,96 @@
+﻿using fluent_behaviour_tree;
+using fluent_behaviour_tree.Nodes;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace tests
+{
+    public class SelectorNodeTests
+    {
+        SelectorNode testObject;
+
+        void Init()
+        {
+            testObject = new SelectorNode();
+        }
+
+        [Fact]
+        public void runs_the_first_node_if_it_succeeds()
+        {
+            Init();
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Success);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Success, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Never());
+        }
+
+        [Fact]
+        public void stops_on_the_first_node_when_it_is_running()
+        {
+            Init();
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Running);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Running, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Never());
+        }
+
+        [Fact]
+        public void runs_the_second_node_if_the_first_fails()
+        {
+            Init();
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Failure);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+            mockChild2
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Success);
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Success, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Once());
+        }
+
+
+    }
+}
+
