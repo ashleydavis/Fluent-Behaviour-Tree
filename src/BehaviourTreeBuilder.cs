a4be0abe33c5ea9674f9ca2babc2501bc8b9ca59
Ashley Davis <ashley@codecapers.com.au>
2015-10-16 13:58:58 +1000

    Added BehaviourTreeBuilder that can create an ActionNode.

---
+++ src/BehaviourTreeBuilder.cs
@@ -0,0 +1,22 @@
+﻿using fluent_behaviour_tree.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fluent_behaviour_tree
+{
+    /// <summary>
+    /// Fluent API for building a behaviour tree.
+    /// </summary>
+    public class BehaviourTreeBuilder
+    {
+        /// <summary>
+        /// Create an action node.
+        /// </summary>
+        public static IBehaviourTreeNode Do(string name, Func<TimeData, BehaviourTreeStatus> fn)
+        {
+            return new ActionNode(name, fn);
+        }
+    }
+}
---
+++ tests/BehaviourTreeBuilderTests.cs
@@ -0,0 +1,39 @@
+﻿using fluent_behaviour_tree;
+using fluent_behaviour_tree.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace tests
+{
+    public class BehaviourTreeBuilderTests
+    {
+        BehaviourTreeBuilder testObject;
+
+        void Init()
+        {
+            testObject = new BehaviourTreeBuilder();
+        }
+
+        [Fact]
+        public void can_create_action_node()
+        {
+            Init();
+
+            var invokeCount = 0;
+
+            var node = BehaviourTreeBuilder
+                .Do("some-node", t =>
+                {
+                    ++invokeCount;
+                    return BehaviourTreeStatus.Running;
+                });
+
+            Assert.IsType<ActionNode>(node);
+            Assert.Equal(BehaviourTreeStatus.Running, node.Tick(new TimeData()));
+            Assert.Equal(1, invokeCount);
+        }
+    }
+}
