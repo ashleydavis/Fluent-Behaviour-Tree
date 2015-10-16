f0d4d489b9829cb607de59be77c07d0953e53748
Ashley Davis <ashley@codecapers.com.au>
2015-10-16 13:46:34 +1000

    Added ParallelNode.

---
+++ src/Nodes/ParallelNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fluent_behaviour_tree.Nodes
+{
+    /// <summary>
+    /// Runs childs nodes in parallel.
+    /// </summary>
+    public class ParallelNode : IBehaviourTreeNode
+    {
+        /// <summary>
+        /// List of child nodes.
+        /// </summary>
+        private List<IBehaviourTreeNode> children = new List<IBehaviourTreeNode>();
+
+        /// <summary>
+        /// Number of child failures required to terminate with failure.
+        /// </summary>
+        private int numRequiredToFail;
+
+        /// <summary>
+        /// Number of child successess require to terminate with success.
+        /// </summary>
+        private int numRequiredToSucceed;
+
+        public ParallelNode(int numRequiredToFail, int numRequiredToSucceed)
+        {
+            this.numRequiredToFail = numRequiredToFail;
+            this.numRequiredToSucceed = numRequiredToSucceed;
+        }
+
+        public BehaviourTreeStatus Tick(TimeData time)
+        {
+            var numChildrenSuceeded = 0;
+            var numChildrenFailed = 0;
+
+            foreach (var child in children)
+            {
+                var childStatus = child.Tick(time);
+                switch (childStatus)
+                {
+                    case BehaviourTreeStatus.Success: ++numChildrenSuceeded; break;
+                    case BehaviourTreeStatus.Failure: ++numChildrenFailed; break;
+                }
+            }
+
+            if (numRequiredToSucceed > 0 && numChildrenSuceeded >= numRequiredToSucceed)
+            {
+                return BehaviourTreeStatus.Success;
+            }
+
+            if (numRequiredToFail > 0 && numChildrenFailed >= numRequiredToFail)
+            {
+                return BehaviourTreeStatus.Failure;
+            }
+
+            return BehaviourTreeStatus.Running;
+        }
+
+        public void AddChild(IBehaviourTreeNode child)
+        {
+            children.Add(child);
+        }
+    }
+}
---
+++ tests/ParallelNodeTests.cs
@@ -0,0 +1,151 @@
+﻿using fluent_behaviour_tree;
+using fluent_behaviour_tree.Nodes;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace tests
+{
+    public class ParallelNodeTests
+    {
+        ParallelNode testObject;
+
+        void Init(int numFailures = 0, int numSuccesses = 0)
+        {
+            testObject = new ParallelNode(numFailures, numSuccesses);
+        }
+
+        [Fact]
+        public void runs_all_nodes_in_order()
+        {
+            Init();
+
+            var time = new TimeData();
+
+            var callOrder = 0;
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Running)
+                .Callback(() =>
+                {
+                    Assert.Equal(1, ++callOrder);
+                });
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+            mockChild2
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Running)
+                .Callback(() =>
+                 {
+                     Assert.Equal(2, ++callOrder);
+                 });
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Running, testObject.Tick(time));
+
+            Assert.Equal(2, callOrder);
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Once());
+        }
+
+        [Fact]
+        public void fails_when_required_number_of_children_fail()
+        {
+            Init(2, 2);
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Failure);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+            mockChild2
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Failure);
+
+            var mockChild3 = new Mock<IBehaviourTreeNode>();
+            mockChild3
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Running);
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+            testObject.AddChild(mockChild3.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Failure, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Once());
+            mockChild3.Verify(m => m.Tick(time), Times.Once());
+        }
+
+        [Fact]
+        public void succeeeds_when_required_number_of_children_succeed()
+        {
+            Init(2, 2);
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Success);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+            mockChild2
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Success);
+
+            var mockChild3 = new Mock<IBehaviourTreeNode>();
+            mockChild3
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Running);
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+            testObject.AddChild(mockChild3.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Success, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Once());
+            mockChild3.Verify(m => m.Tick(time), Times.Once());
+        }
+
+        [Fact]
+        public void continues_to_run_if_required_number_children_neither_succeed_or_fail()
+        {
+            Init(2, 2);
+
+            var time = new TimeData();
+
+            var mockChild1 = new Mock<IBehaviourTreeNode>();
+            mockChild1
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Success);
+
+            var mockChild2 = new Mock<IBehaviourTreeNode>();
+            mockChild2
+                .Setup(m => m.Tick(time))
+                .Returns(BehaviourTreeStatus.Failure);
+
+            testObject.AddChild(mockChild1.Object);
+            testObject.AddChild(mockChild2.Object);
+
+            Assert.Equal(BehaviourTreeStatus.Running, testObject.Tick(time));
+
+            mockChild1.Verify(m => m.Tick(time), Times.Once());
+            mockChild2.Verify(m => m.Tick(time), Times.Once());
+        }
+    }
+}
